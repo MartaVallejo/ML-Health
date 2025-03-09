1978ac1af8c8eee1884822d0b731c92d29a2df9d
Dr Marta Vallejo <martavallejo@gmail.com>
2025-03-09 01:39:57 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
+function matchProjectImageHeight() {
+    document.querySelectorAll('.project-box').forEach(project => {
+        let textHeight = project.querySelector('.project-content').offsetHeight;
+        let imageContainer = project.querySelector('.project-image');
 
+        if (imageContainer) {
+            imageContainer.style.height = textHeight + "px"; // Force height match
+        }
+    });
+}
+
+// Run function on page load and when window resizes
+window.addEventListener("load", matchProjectImageHeight);
+window.addEventListener("resize", matchProjectImageHeight);
